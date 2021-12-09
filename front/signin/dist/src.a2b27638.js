810d59a9749952c18b842ad70c9fe87dfe2c238b
Hwang-Sangjin <52124805+Hwang-Sangjin@users.noreply.github.com>
2021-12-09 22:41:45 +0900

    12 09

--- front/signin/dist/src.a2b27638.js
+++ front/signin/dist/src.a2b27638.js
@@ -2762,7 +2762,7 @@ function router() {
 
 window.addEventListener('hashchange', router);
 document.addEventListener('DOMContentLoaded', router);
-},{"./store":"src/store.js","./page/login":"src/page/login.js","./page/profile":"src/page/profile.js","./page/page-not-found":"src/page/page-not-found.js","./page/signup":"src/page/signup.js"}],"../../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{"./store":"src/store.js","./page/login":"src/page/login.js","./page/profile":"src/page/profile.js","./page/page-not-found":"src/page/page-not-found.js","./page/signup":"src/page/signup.js"}],"../../../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -2790,7 +2790,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "14331" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "8528" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
@@ -2966,5 +2966,5 @@ function hmrAcceptRun(bundle, id) {
     return true;
   }
 }
-},{}]},{},["../../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js","src/index.js"], null)
+},{}]},{},["../../../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js","src/index.js"], null)
 //# sourceMappingURL=/src.a2b27638.js.map
