899b2b6e012f3749b49c1e09009aad6e6450380d
BUMSEOK SEO <sbseok12@gmail.com>
2021-12-18 08:21:50 +0900

    feat: 회원가입 기능 구현

--- front/signin/src/page/signup.js
+++ front/signin/src/page/signup.js
@@ -1,6 +1,7 @@
 
 import template from './signup.template';
-import { TextField, PasswordField, AddressField } from './views';
+import { TextField, PasswordField, RadioSelectField } from './views';
+import axios  from 'axios';
 
 export default class Signup {
   #template = template;
@@ -20,27 +21,62 @@ export default class Signup {
   }
 
   #initialize = () => {
-    const nameField = new TextField('#required-fields', {
-      id: 'name', label: '이름', type: 'text', placeholder: '이름을 입력해주세요', require: true,
-    });
-    const idField = new TextField('#required-fields', {
-      id: 'id', label: '아이디', type: 'text', placeholder: '아이디를 입력해주세요', require: true,
-    });
-    const passwordField = new TextField('#required-fields', {
-      id: 'password', label: '비밀번호', placeholder: '비밀번호를 입력해주세요',
+    const userNameField = new TextField('#required-fields', {
+      id: 'userName', label: '이름', type: 'text', name: 'userName', placeholder: '이름을 입력해주세요', require: true,
     });
     const emailField = new TextField('#required-fields', {
-      id: 'email', label: '이메일', type: 'email', placeholder: '이메일을 입력해주세요', require: true,
+      id: 'email', label: '이메일', type: 'email', name: 'email', placeholder: '이메일을 입력해주세요', require: true,
+    });
+    const passwordField = new PasswordField('#required-fields', {
+      id: 'password', label: '비밀번호', name: 'password', placeholder: '비밀번호를 입력해주세요',
+    });
+    const radioField = new RadioSelectField('#required-fields', {
+      id: 'radio', label:'견주인가요?', name: 'isOwner',
     });
 
-    this.#fields.push(nameField);
-    this.#fields.push(idField);
+    this.#fields.push(userNameField);
     this.#fields.push(emailField);
     this.#fields.push(passwordField);
+    this.#fields.push(radioField);
   }
 
   #validFieldMonitor = () => {
-    
+  }
+
+  #onSubmit = e => {
+    e.preventDefault();
+    console.log(e.target.isOwner.value);
+    let payload = { email : e.target.email.value,  userName: e.target.userName.value, password: e.target.password.value};
+    axios.post('https://doggu.kr/api/accounts', payload)
+    .then(res => {
+      console.log(res);
+      location.href = '/#/pick';
+    })
+    .catch(e => {
+      // console.log(e);
+      // let errorMessages;
+      // e.errors.forEach(errorMessage => {
+      //   errorMessages += errorMessage + "\n";
+      // })
+      // alert("아래의 사항에 대해서 수정이 필요합니다" + "\n" + errorMessages);
+    })
+
+    let requestBody = { "registerNumber" : e.target.registerNumber.value, "ownerName" : e.target.ownerName.value };
+
+    axios.post('https://doggu.kr/api/accounts/dog-info', requestBody, {
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer' + sessionStorage.getItem('token')
+      }
+    })
+    .then((res) => {
+      console.log(res);
+      location.href = '/#/pick';
+    })
+    .catch((e) => {
+      console.log(e);
+    });
+    return false;
   }
 
   render = () => {
@@ -49,7 +85,7 @@ export default class Signup {
       field.render(true);
     });
 
-    //this.container.addEventListener('submit', this.onSubmit);
+    this.#container.addEventListener('submit', this.#onSubmit);
   }
 }
 
